8ede858508cb35ffc6f0db37d29aa017dfe2bcc8
coder <79178809+c0deblack@users.noreply.github.com>
2023-05-17 10:26:39 -0400

    Created custon singly linked list

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -16,15 +16,75 @@ import java.util.List;
  *  That is, if the kth node of the first linked list is the exact same node (by reference)
  *  as the jth node of the second linked list, then they are intersecting.
  * </pre>
+ *
  */
 public class Application {
+    /**
+     * Basic Node implementation
+     */
     private static class Node {
-        private final int value;
+        private int value;
+
+        private Node next = null;
+
+        Node(){}
+
         Node(int v) {
             this.value = v;
         }
         public int value(){
             return this.value;
+        }
+
+        public void value(int i){
+            this.value = i;
+        }
+
+        public Node next(){
+            return this.next;
+        }
+
+        public void next(Node n){
+            this.next = n;
+        }
+
+    }
+
+    /**
+     * Basic Singly Linked List implementation.
+     */
+    private static class SinglyLinkedList {
+        private Node head = null;
+        private Node tail = null;
+        private long size = 0;
+
+        public long size(){
+            return this.size;
+        }
+
+        public long add(Node n){
+            if (head == null) {
+                this.head = n;
+                this.tail = this.head;
+            } else {
+                tail.next(n);
+                tail = tail.next;
+            }
+            return ++this.size;
+        }
+
+        public Node get(int i){
+            if (i > size) {
+                throw new IndexOutOfBoundsException("Index out of bounds");
+            }
+
+            Node current = this.head;
+            for(int n = 0; n < i; n++){
+                if (current == null) break;
+                current = current.next;
+            }
+
+            return current;
         }
     }
 
@@ -34,33 +94,35 @@ public class Application {
         // generate nodes that will be stored in the linked list
         for(int i = 0; i < nodes.length; i++){
             nodes[i] = new Node(i);
+            System.out.println("Node #" + i + " address = " + nodes[i]);
         }
+        System.out.println();
 
         // create the first linked list
-        LinkedList<Node> linkNodes1 = new LinkedList<>(){
+        SinglyLinkedList linkNodes1 = new SinglyLinkedList(){
             {
                 this.add(nodes[0]);
-                this.add(nodes[1]);
                 this.add(nodes[2]);
-                this.add(nodes[3]);
                 this.add(nodes[4]);
 
-                // nodes[5] and nodes[6] will also be in linkedList2
+                // shared nodes
+                this.add(nodes[1]);
+                this.add(nodes[3]);
                 this.add(nodes[5]);
                 this.add(nodes[6]);
 
             }
         };
 
-        LinkedList<Node> linkedList2 = new LinkedList<>(){
+        SinglyLinkedList linkedList2 = new SinglyLinkedList(){
             {
-                // nodes[5] and nodes[6] are shared with linkedList1
+                // shared nodes
+                this.add(nodes[1]);
                 this.add(nodes[5]);
                 this.add(nodes[6]);
+                this.add(nodes[3]);
 
                 // other nodes
-                this.add(nodes[1]);
-                this.add(nodes[3]);
                 this.add(nodes[8]);
             }
         };
@@ -68,7 +130,7 @@ public class Application {
         System.out.println(getIntersectingNodes(linkNodes1, linkedList2));
     }
 
-    private static final List<Node> getIntersectingNodes(LinkedList<Node> l1, LinkedList<Node> l2){
+    private static final List<Node> getIntersectingNodes(SinglyLinkedList l1, SinglyLinkedList l2){
         LinkedList<Node> output = new LinkedList<>();
 
         System.out.println("INTERSECTIONS");
